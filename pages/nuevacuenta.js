9917dddbc05d384df41c7974ddb71ce2bb960c46
DESKTOP-SB43F9M\was <wascasla@gmail.com>
2020-08-05 21:32:34 -0300

    ADD pages login nuevacuneta pedidos etc

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const nuevacuenta = () => {
+    return (
+        <>
+            <Layout>
+                <h1>nueva cuenta</h1>
+            </Layout>
+        </>
+    )
+}
+
+export default nuevacuenta
