d36196c41af46842edadc0b92c2eec741ec15055
DESKTOP-SB43F9M\was <wascasla@gmail.com>
2020-08-06 20:36:33 -0300

    ADD provider client

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import cliente from '../config/apollo';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={cliente}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
