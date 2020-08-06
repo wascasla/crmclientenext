c20814e671c954fc553eba4452a9b9313985354d
DESKTOP-SB43F9M\was <wascasla@gmail.com>
2020-08-06 20:24:38 -0300

    ADD validation yup nuevacuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -13,6 +13,12 @@ const nuevacuenta = () => {
             email: '',
             password: ''
         },
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('El nombre es obligatorio'),
+            apellido: Yup.string().required('El apellido es obligatorio'),
+            email: Yup.string().email('El email no es válido').required('El email es obligatorio'),
+            password: Yup.string().required("El password no puede ir vacio").min(6, 'El password debe ser de al menos 6 caracteres')
+        }),
         onSubmit: valores => {
             console.log('enviando');
             console.log(valores);
@@ -26,71 +32,109 @@ const nuevacuenta = () => {
 
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-sm">
-                        <form 
+                        <form
                             className="bg-white rounded shadow-md px-8 pt-8 pb-8 mb-4"
                             onSubmit={formik.handleSubmit}
-                            >
+                        >
 
-                            
-                        <div className="mb-4">
+
+                            <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
                                     Nombre
                                 </label>
-                                <input 
+                                <input
                                     className="shadow appearence-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="nombre"
                                     type="text"
                                     placeholder="Nombre usuario"
                                     value={formik.values.nombre}
                                     onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
 
                                 />
                             </div>
+
+                            {formik.touched.nombre && formik.errors.nombre ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                    <p className="font-bold">Error</p>
+                                    <p> {formik.errors.nombre} </p>
+                                </div>
+
+                            ) : null}
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
                                     Apellido
                                 </label>
-                                <input 
+                                <input
                                     className="shadow appearence-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="apellido"
                                     type="text"
                                     placeholder="Apellido usuario"
                                     value={formik.values.apellido}
                                     onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
 
                                 />
                             </div>
+
+                            {formik.touched.apellido && formik.errors.apellido ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                    <p className="font-bold">Error</p>
+                                    <p> {formik.errors.apellido} </p>
+                                </div>
+
+                            ) : null}
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                                     Email
                                 </label>
-                                <input 
+                                <input
                                     className="shadow appearence-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="email"
                                     type="email"
                                     placeholder="Email usuario"
                                     value={formik.values.email}
                                     onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
 
                                 />
                             </div>
+
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                    <p className="font-bold">Error</p>
+                                    <p> {formik.errors.email} </p>
+                                </div>
+
+                            ) : null}
 
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                     Password
                                 </label>
-                                <input 
+                                <input
                                     className="shadow appearence-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="password"
                                     type="password"
                                     placeholder="Password usuario"
                                     value={formik.values.password}
                                     onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
 
                                 />
                             </div>
 
-                            <input 
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                    <p className="font-bold">Error</p>
+                                    <p> {formik.errors.password} </p>
+                                </div>
+
+                            ) : null}
+
+                            <input
                                 type="submit"
                                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
                                 value="Crear Cuenta"
