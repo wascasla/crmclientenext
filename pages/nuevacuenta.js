75e42d175a7ca0619603ef79f7b135e351333fac
wascasla <wascasla@hotmail.com>
2020-08-06 12:35:24 -0300

    ADD formik pages: nuevacuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const nuevacuenta = () => {
+
+    // validacion del formulario
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            apellido: '',
+            email: '',
+            password: ''
+        },
+        onSubmit: valores => {
+            console.log('enviando');
+            console.log(valores);
+        }
+    });
+
     return (
         <>
             <Layout>
@@ -9,7 +26,11 @@ const nuevacuenta = () => {
 
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-sm">
-                        <form className="bg-white rounded shadow-md px-8 pt-8 pb-8 mb-4">
+                        <form 
+                            className="bg-white rounded shadow-md px-8 pt-8 pb-8 mb-4"
+                            onSubmit={formik.handleSubmit}
+                            >
+
                             
                         <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
@@ -20,6 +41,8 @@ const nuevacuenta = () => {
                                     id="nombre"
                                     type="text"
                                     placeholder="Nombre usuario"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
 
                                 />
                             </div>
@@ -32,6 +55,8 @@ const nuevacuenta = () => {
                                     id="apellido"
                                     type="text"
                                     placeholder="Apellido usuario"
+                                    value={formik.values.apellido}
+                                    onChange={formik.handleChange}
 
                                 />
                             </div>
@@ -44,6 +69,8 @@ const nuevacuenta = () => {
                                     id="email"
                                     type="email"
                                     placeholder="Email usuario"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
 
                                 />
                             </div>
@@ -57,6 +84,8 @@ const nuevacuenta = () => {
                                     id="password"
                                     type="password"
                                     placeholder="Password usuario"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
 
                                 />
                             </div>
