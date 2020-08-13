12129f324617a8efbde3ff71e427e8c23feb84bb
wascasla <wascasla@hotmail.com>
2020-08-13 11:16:40 -0300

    ADD apollo-link-context

--- config/apollo.js
+++ config/apollo.js
@@ -1,13 +1,28 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import fetch from 'node-fetch';
+import { setContext } from 'apollo-link-context';
+
+// tiene la configuracion de a donde nos vamos a contectar para obtener los datos
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/',
+    fetch
+});
+
+// modificamos el header
+const authLink = setContext((_, { headers }) => {
+
+    // Leer el storage almacenado
+    const token = localStorage.getItem('token');
+    return {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+    }
+});
 
 const client = new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000/',
-        fetch
-    })
+    link: authLink.concat(httpLink)
 });
 
 export default client;
